7136d5cda5af767ae6db86b1d752587db331b3d1
WerdnaLes <werdnales@gmail.com>
2022-10-08 11:37:10 +0300

    Day 1

--- app/src/main/java/com/example/geoquiz/challenges/Challenge1Activity.kt
+++ app/src/main/java/com/example/geoquiz/challenges/Challenge1Activity.kt
@@ -1,18 +1,18 @@
-package com.example.geoquiz
+package com.example.geoquiz.challenges
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import com.example.geoquiz.R
 
-class MainActivity : AppCompatActivity() {
+class Challenge1Activity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_chall1)
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
